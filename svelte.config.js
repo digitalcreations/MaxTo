4f095b8aefdb2a7b6726fbd8aed44039da50f82a
Vegard Andreas Larsen <vegard@beat.no>
2021-06-13 10:16:27 +0200

    Setup @sveltejs/adapter-static.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
 
